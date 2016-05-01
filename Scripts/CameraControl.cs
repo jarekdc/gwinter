d67ef614400be6251e6af9eade71c62d40fc20d5
Jarek Polak <Jarek Polak>
2016-05-01 21:16:03 +0200

    basic movement and rotation

---
+++ Scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+	
+	public GameObject player;
+    public float mFollowRate = 5.0f;
+    public bool followPlayer = true;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		Camera.main.orthographicSize = 2.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+        if (followPlayer)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position + new Vector3(0, 0, -10), Time.deltaTime * mFollowRate);
+        }       
+	}
+
+    public void setFollowPlayer(bool val)
+    {
+        followPlayer = val;
+    }
+ }
---
+++ Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 3f;
+    public bool moving;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate () {
+        // Basic movement
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(speed * inputX, speed * inputY, 0) * Time.deltaTime;
+        transform.Translate(movement);
+
+        if(inputX == 0 && inputY == 0)
+        {
+            moving = false;
+        }
+        else
+        {
+            moving = true;
+        }
+    }
+}
