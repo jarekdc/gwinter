5b55969c47278c9493b3a66e45d2e5d162bbcdbd
Jarek Polak <Jarek Polak>
2016-05-13 00:09:11 +0200

    basic blood particles

--- Scripts/BulletHit.cs
+++ Scripts/BulletHit.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class BulletHit : MonoBehaviour {
 
     public int bulletDamage = 10;
+    public ParticleSystem bloodShotgun;
 
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Enemy")
         {
             coll.gameObject.GetComponent<Health>().Damage(bulletDamage);
-            Destroy (gameObject);        
+            Instantiate(bloodShotgun, transform.position, Quaternion.Euler(transform.rotation.eulerAngles.z + 180f, 270f, 180f));
+            Destroy (gameObject);    
         }        
     }
 }
---
+++ Scripts/DestroyObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour {
+
+    public float lifetime;
+
+	// Use this for initialization
+	void Update ()
+    {
+        Destroy(gameObject, lifetime);
+	}
+
+}
