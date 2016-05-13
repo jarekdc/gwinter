17e0678321bdd6816d489f047ab9e0f4fac98128
Jarek Polak <Jarek Polak>
2016-05-13 16:04:45 +0200

    enemy trig -> coll, fixes

--- Scripts/BulletHit.cs
+++ Scripts/BulletHit.cs
@@ -6,13 +6,13 @@ public class BulletHit : MonoBehaviour {
     public int bulletDamage = 10;
     public ParticleSystem bloodShotgun;
 
-    void OnTriggerEnter2D(Collider2D coll)
+    void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Enemy")
         {
             coll.gameObject.GetComponent<Health>().Damage(bulletDamage);
             Instantiate(bloodShotgun, transform.position, Quaternion.Euler(transform.rotation.eulerAngles.z + 180f, 270f, 180f));
-            Destroy (gameObject);    
+            Destroy (gameObject, 0.1f);    
         }        
     }
 }
